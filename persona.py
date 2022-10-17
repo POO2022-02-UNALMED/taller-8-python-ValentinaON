8a7a1b775dac9f687726ab6615facc3a6a22f725
ValentinaON <111089153+ValentinaON@users.noreply.github.com>
2022-10-17 14:13:58 -0500

    Creacion proyecto deportista
    Se crean las diferentes clases, atributos y métodos solicitados

---
+++ persona.py
@@ -0,0 +1,31 @@
+class Persona():
+    def __init__(self, nombre, edad, altura, sexo):
+        self._nombre = nombre
+        self.edad = edad
+        self.altura = altura
+        self.sexo = sexo
+
+def getNombre(self):
+    return self._nombre
+
+def setNombre(self,nombre):
+    self._nombre = nombre
+
+def getEdad(self):
+    return self._edad
+
+def setEdad(self,edad):
+    self._edad = edad
+
+def getAltura(self):
+    return self._altura
+
+def setAltura(self,altura):
+    self._altura = altura
+
+def getSexo(self):
+    return self._sexo
+
+def setSexo(self,sexo):
+    self._sexo = sexo
+
